b71c20b0007a1fc4d493aaac6f8d7bd243bcbf21
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-11-29 16:38:56 +0300

    useAsyncData Options - using pick shorthand instead of transform to pick out only a few keys that we need in the object

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -6,6 +6,8 @@ const {data} = await useAsyncData(
     `/movies/${route.params.id}`,
     () => {
       return $fetch(`http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`);
+    },{
+      pick: ['Plot', 'Title']
     }
 );
 
