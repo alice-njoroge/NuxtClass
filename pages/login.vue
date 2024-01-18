868fe66c4540ef9465cbc23571a4714731dd873e
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-01-18 10:48:58 +0300

    what if we want a page to not have the default layout?? a page like login or register.vue??

---
+++ pages/login.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+definePageMeta({
+  layout: 'plain'
+})
+
+</script>
+
+<template>
+<form>
+  <label>
+    Username:
+    <input type="text" >
+  </label>
+  <label>
+    Password:
+    <input type="password">
+  </label>
+  <button> Submit</button>
+</form>
+</template>
+
+<style scoped>
+
+</style>
